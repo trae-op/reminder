9825c32b8af52ed55b4e04f855fd06416714d50c
oleg.pronchenko <traeop@gmail.com>
2025-07-28 11:40:46 +0300

    at first we should start check sleep without timer because it should fix the status sleep

--- src/main/sleep/ipc.ts
+++ src/main/sleep/ipc.ts
@@ -1,14 +1,12 @@
 import { ipcMainOn, ipcWebContentsSend } from "../@shared/utils.js";
 import { getWindow } from "../@shared/control-window/receive.js";
-import { sleepInterval } from "./service.js";
+import { checkSleep, sleepInterval } from "./service.js";
 
 export function registerIpc(): void {
   ipcMainOn("sleep", () => {
     const mainWindow = getWindow<TWindows["main"]>("window:main");
     if (mainWindow !== undefined) {
-      ipcWebContentsSend("sleepOff", mainWindow.webContents, {
-        ok: false,
-      });
+      checkSleep(mainWindow);
       sleepInterval(mainWindow);
     }
   });
--- src/main/sleep/service.ts
+++ src/main/sleep/service.ts
@@ -6,6 +6,8 @@ import { TResponseSleep } from "./types.js";
 import { ipcWebContentsSend } from "../@shared/utils.js";
 import { getElectronStorage } from "../@shared/store.js";
 
+let interval: NodeJS.Timeout | undefined = undefined;
+
 async function sleepOff<R extends TResponseSleep>(): Promise<R | undefined> {
   const response = await get<R>(
     `${restApi.urls.base}${restApi.urls.baseApi}${restApi.urls.auth.base}${restApi.urls.auth.sleep}`
@@ -23,19 +25,23 @@ async function sleepOff<R extends TResponseSleep>(): Promise<R | undefined> {
 }
 
 export async function sleepInterval(window: BrowserWindow) {
-  const interval = setInterval(async () => {
-    ipcWebContentsSend("sleepOff", window.webContents, {
-      ok: false,
-    });
-
-    const response = await sleepOff();
-    ipcWebContentsSend("sleepOff", window.webContents, {
-      ok: (response !== undefined && response.ok) || true,
-    });
-
-    const authToken = getElectronStorage("authToken");
-    if (authToken === undefined) {
-      clearInterval(interval);
-    }
+  interval = setInterval(() => {
+    checkSleep(window);
   }, timers.intervalCheckSleep);
+}
+
+export async function checkSleep(window: BrowserWindow) {
+  ipcWebContentsSend("sleepOff", window.webContents, {
+    ok: false,
+  });
+
+  const response = await sleepOff();
+  ipcWebContentsSend("sleepOff", window.webContents, {
+    ok: (response !== undefined && response.ok) || true,
+  });
+
+  const authToken = getElectronStorage("authToken");
+  if (authToken === undefined && interval !== undefined) {
+    clearInterval(interval);
+  }
 }
